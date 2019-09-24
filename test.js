56b0cb7ab294f69b302d38d72f822d9422fe65aa
wuzhenghui11 <wb_wuzhenghui@zhongan.com>
2019-09-24 14:50:16 +0800

    m

--- test.js
+++ test.js
@@ -2,3 +2,17 @@ const path = require('path');
 console.log(__dirname);
 console.log(path.resolve(__dirname, '../../work/zhongan/20190722_463_1/src/main/resources/static/'));
 console.log(path.join('/foo', 'bar', 'baz/asdf', 'quux'));
+
+const someAsyncThing = function() {
+	return new Promise(function(resolve, reject) {
+		// 下面一行会报错，因为x没有声明
+		resolve(x + 2);
+	});
+};
+someAsyncThing().then(function() {
+	console.log('everything is great');
+});
+setTimeout(() => { console.log(123) }, 2000);
+process.on('unhandledRejection', function (err, p) {
+	throw err;
+});
