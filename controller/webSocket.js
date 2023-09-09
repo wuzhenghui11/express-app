370daa61d3be63065d0612133af3789ac32b34a1
wuzhenghui11 <wuzhenghui11@163.com>
2023-09-09 18:59:49 +0800

    modify:.gitignore

--- controller/webSocket.js
+++ controller/webSocket.js
@@ -3,14 +3,15 @@ const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: 8282 })
 
 wss.on('connection', (ws) => {
+  ws.on('error', console.error)
 
   ws.on('message', function message (data) {
     // 所有连接发送的消息
     // console.log(wss.clients)
     wss.clients.forEach((msg) => {
       console.log(msg)
-      // ws.send('server message: ha')
     })
+    // ws.send('server message: ha')
   })
 })
 
