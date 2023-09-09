c62a6da8ef82f62d31a228b45c2aebec86bea9aa
wuzhenghui11 <wuzhenghui11@163.com>
2023-09-09 19:30:48 +0800

    update

--- utils/nodeRun.js
+++ utils/nodeRun.js
@@ -1,3 +1,10 @@
+#!/usr/bin/env node
+// console.log('这个只是unix类的操作系统才有意义');
+// console.log('告诉操作系统执行这个脚本的时候，首先会到env设置里查找node的安装路径，再调用对应路径下的解释器程序完成操作。');
+
+// console.log('hello', process.argv[2]);
+
+
 const path = require('path');
 console.log(__dirname);
 console.log(path.resolve(__dirname, '../../work/zhongan/20190722_463_1/src/main/resources/static/'));
